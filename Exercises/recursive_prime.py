c6f09d78ff158b291e67bb80dcdc1f6b22c25eba
gormac23 <colin.gorman23@mail.dcu.ie>
2020-11-10 22:18:23 +0000

    Change made to recursive_prime.py
    Added a main function so as to allow testing of primes from terminal

--- Exercises/recursive_prime.py
+++ Exercises/recursive_prime.py
@@ -7,3 +7,11 @@ def is_prime(n, a=2):
         return False
     else:
         return is_prime(n,a+1)
+
+def main():
+    num = int(input("Enter number you want to test is prime ...\n"))
+    print(is_prime(num))
+
+
+if __name__ == '__main__':
+    main()
